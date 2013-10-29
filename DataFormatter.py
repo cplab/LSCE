ecc6b351727b305374091edb1271d366a18d1318
Nick Beaumont <nob8@cornell.edu>
2013-10-29 14:36:38 -0400

    Added ability to append data to existing hdf5 files, various fault tolerance behavior, attributes for shape and data type on datasets. The raw_data group contains all imported arrays, and records number of entries in the "count" attribute.

--- DataFormatter.py
+++ DataFormatter.py
@@ -1,31 +1,46 @@
-import scipy.io
 import numpy as np
 import h5py
 import os
 import time
 
-import importer
+
+def timestamp():
+    return time.strftime("_%H:%M:%S:0000_%m-%d-%Y_GMT", time.gmtime())
+
 
 def formatData(fileDir, name, **options):
-    """Loads raw numpy arrays from a folder and saves them in hdf5 file format. Tagging information about the arrays may be supplied
-        in a seperate file.
+    """Loads raw numpy arrays from a folder and saves them in hdf5 file format.
+        Each array will be given its own dataset under the group 'raw_data'.
 
         Args:
             fileDir: the absolute path to a directory containing one or more numpy files.
             *options: An optional list of command-line switches or paths to additional files containing tagging information
     """
     #Decode launch options
-    file_access_mode='r+' if ('-a' in options) else "w"
-    timestamp=time.strftime("_%H:%M:%S:0000_%m-%d-%Y_GMT", time.gmtime()) if ('-t' in options) else ''
-    f=h5py.File(name+".hdf5", "w")
-    data_dir=f.create_group("raw_data" + timestamp)
+    file_access_mode = 'r+' if ('-a' in options) else "w"
+    #timestamp = time.strftime("_%H:%M:%S:0000_%m-%d-%Y_GMT", time.gmtime()) if ('-t' in options) else ''
+    f = h5py.File(name+".hdf5", file_access_mode)
+    if(not ('raw_data' in f)):
+        data_dir = f.create_group("raw_data")
+    else:
+        data_dir = f['raw_data']
     os.chdir(fileDir)
     for files in os.listdir("."):
         if(files.endswith(".npy")):
-            tmp=np.load(files)
-            dset=f.create_dataset(files[0:files.index(".npy")], data=tmp)
+            tmp = np.load(files)
+            if(files[0:files.index(".npy")] in data_dir):
+                conflict_name = (files[0:files.index(".npy")] + "_conflicted_copy_" + timestamp())
+                dset = data_dir.create_dataset(conflict_name, data=tmp)
+            else:
+                dset = data_dir.create_dataset(files[0:files.index(".npy")], data=tmp)
+
+            dset.attrs.create("shape", np.shape(tmp))
+            dset.attrs.create("dtype", tmp.dtype)
+            numsets = data_dir.attrs.get("count")
+            data_dir.attrs.modify("count", [numsets[0] + 1])
             f.flush()
+    data_dir.attrs.modify("last_modified", [timestamp()])
     #At this point, the raw datasets have each been imported from the .npy files.
-    #We now check formatting options for important attributes, 
+    #We now check formatting options for important attributes
     f.close()
     return f
