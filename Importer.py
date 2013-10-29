e575968d97e9a56cc60f492001d2be7e912fa74b
Detian Shi <shi.detian@gmail.com>
2013-10-29 16:05:49 -0400

    implement smash2

--- Importer.py
+++ Importer.py
@@ -1,8 +1,33 @@
 import scipy.io
+import array
 import numpy as np
 from math import ceil
 import os
 
+
+electrodes = [
+    21, 31, 41, 51, 61, 71, 12, 22, 32, 42, 52, 62, 72, 82, 13, 23, 33,
+    43, 53, 63, 73, 83, 14, 24, 34, 44, 54, 64, 74, 84, 15, 25, 35, 45,
+    55, 65, 75, 85, 16, 26, 36, 46, 56, 66, 76, 86, 17, 27, 37, 47, 57,
+    67, 77, 87, 28, 38, 48, 58, 68, 78
+]
+
+def smash2(filedir, type, fileNum):
+    #TODO use correct OS separators
+    savedir = filedir + '\\' + type
+    filename = savedir+"%03d"%(filenum-1)+".raw"
+    fid = file(filename)
+    fileSize = os.stat(filename)
+    data = array.array("H")
+    data.fromfile(fid, fileSize.st_size/2)
+    data[:] = [(x-32768)*0.0104 for x in data]
+    
+    numelec = 60
+    out = []
+    for i in range(numelec):
+        out.append(data[i:fileSize.st_size/2:numelec])
+    
+    return out
 
 def loadFromMat(filedir, Fs=10e3, rFs=256):
     """Loads folder of matlab data files and saves them in numpy format.
@@ -15,13 +40,6 @@ def loadFromMat(filedir, Fs=10e3, rFs=256):
     """
     pts = [343*Fs+1, 643*Fs]
 
-    electrodes = [
-        21, 31, 41, 51, 61, 71, 12, 22, 32, 42, 52, 62, 72, 82, 13, 23, 33,
-        43, 53, 63, 73, 83, 14, 24, 34, 44, 54, 64, 74, 84, 15, 25, 35, 45,
-        55, 65, 75, 85, 16, 26, 36, 46, 56, 66, 76, 86, 17, 27, 37, 47, 57,
-        67, 77, 87, 28, 38, 48, 58, 68, 78
-    ]
-
     for ii in electrodes:
         mat = scipy.io.loadmat(
             "{0}{2}Electrode_{1}_master.mat".format(filedir, ii, os.sep)
@@ -31,3 +49,9 @@ def loadFromMat(filedir, Fs=10e3, rFs=256):
         np.save("{0}{2}Electrode_{1}_master".format(filedir, ii, os.sep), data)
 
     return filedir
+
+def loadFromRaw(filedir, numFiles=6, type='slice2_', Fs=20e3):
+    for i in range(numFiles):
+        smash2(filedir, type, i)
+        
+    
