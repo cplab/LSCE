097063a1932393a2eff3e9e8012b1fa79feaa5a1
Detian Shi <shi.detian@gmail.com>
2013-11-03 16:47:16 -0500

    import from raw

--- Importer.py
+++ Importer.py
@@ -3,6 +3,7 @@ import array
 import numpy as np
 from math import ceil
 import os
+import tempfile
 
 
 electrodes = [
@@ -12,22 +13,46 @@ electrodes = [
     67, 77, 87, 28, 38, 48, 58, 68, 78
 ]
 
-def smash2(filedir, type, fileNum):
-    #TODO use correct OS separators
-    savedir = filedir + '\\' + type
-    filename = savedir+"%03d"%(filenum-1)+".raw"
+def smash2(filedir, workingdir, type, fileNum):
+    savedir = filedir + os.sep + type
+    filename = savedir+"%04d"%(fileNum)+".raw"
+    print filename
     fid = file(filename)
-    fileSize = os.stat(filename)
-    data = array.array("H")
-    data.fromfile(fid, fileSize.st_size/2)
-    data[:] = [(x-32768)*0.0104 for x in data]
+    #fileSize = os.stat(filename).st_size
+    #data = array.array("H")
+    #data.fromfile(fid, fileSize/2)
+    data = np.fromfile(fid, dtype=np.uint16)
+    
+    #convertedData = [(x-32768)*0.0104 for x in data]
+    data -= 32768
+    data *= 0.0104
     
     numelec = 60
     out = []
     for i in range(numelec):
-        out.append(data[i:fileSize.st_size/2:numelec])
+        #out.append(data[i:data.size:numelec])
+        np.save(workingdir + os.sep + "Electrode_"+str(electrodes[i])+"_"+str(fileNum), data[i:data.size:numelec])
+    return
+
+def mergemat(filename, numFiles, Fs):
+    datatemp = np.load(filename+"0.npy")
     
-    return out
+    L = (2*60)*Fs
+    
+    data = np.zeros((L,numFiles-2))
+    
+    for i in range(numFiles-2):
+        data[:,i+1] = np.load(filename + i+1)
+    
+    L2 = (numFiles-2)*L
+    dataheap = np.reshape(data, (L2,1))
+    
+    data = np.load(filename + str(numFiles-1) + ".npy")
+    dataheap = np.array([[datatemp],[dataheap],[data]])
+    
+    holder = {}
+    holder['dataheap'] = dataheap
+    return holder
 
 def loadFromMat(filedir, Fs=10e3, rFs=256):
     """Loads folder of matlab data files and saves them in numpy format.
@@ -51,7 +76,12 @@ def loadFromMat(filedir, Fs=10e3, rFs=256):
     return filedir
 
 def loadFromRaw(filedir, numFiles=6, type='slice2_', Fs=20e3):
-    for i in range(numFiles):
-        smash2(filedir, type, i)
-        
+    workingdir = tempfile.mkdtemp()
+    for i in xrange(numFiles):
+        smash2(filedir, workingdir, type, i)
     
+    for i in range(60):
+        file = workingdir + os.sep + "Electrode_" + str(electrodes[i]) + "_"
+        holder = mergemat(file, numFiles, Fs)
+        #f = open("{0}{2}{3}{2}Electrode_{1}_master".format(filedir, electrodes[i], os.sep, type) + ".mat", "w")
+        scipy.io.savemat("{0}{2}{3}{2}Electrode_{1}_master".format(filedir, electrodes[i], os.sep, type), holder)
