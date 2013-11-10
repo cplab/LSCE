9e80d9f4c5e6214fade64a8d400860aa9294bb20
fatherbobcat <linkena@gmail.com>
2013-11-10 16:24:59 -0500

    graphs moving with time

--- datavisualization.py
+++ datavisualization.py
@@ -1,30 +1,107 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Nov 05 17:19:33 2013
+from numpy import arange, sin, pi, float, size
 
-@author: CIT-Labs
-"""
+import matplotlib
+matplotlib.use('WXAgg')
+from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg
+from matplotlib.figure import Figure
 
-import numpy as np
-import matplotlib.pyplot as plt
+import wx
 
+class MyFrame(wx.Frame):
+    
+    def __init__(self, parent, id):
+        self.empty=[0,7,56,63]        
+        
+        wx.Frame.__init__(self,parent, id, 'scrollable plot',
+                style=wx.DEFAULT_FRAME_STYLE ^ wx.RESIZE_BORDER,
+                size=(800, 400))
+        self.panel = wx.Panel(self, -1)
 
-#x1 = np.arange(1,10)
-#x2 = np.linspace(0.0, 2.0)
+        self.fig = Figure((5, 4), 75)
+        self.canvas = FigureCanvasWxAgg(self.panel, -1, self.fig)
+        self.scroll_range = 400
+        self.canvas.SetScrollbar(wx.HORIZONTAL, 0, 5,
+                                 self.scroll_range)
+        self.graphs = []                                 
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(self.canvas, -1, wx.EXPAND)
 
-#y1 = np.cos(2 * np.pi * x1) * np.exp(-x1)
-#y2 = np.cos(2 * np.pi * x2)
+        self.panel.SetSizer(sizer)
+        self.panel.Fit()
 
-y1 = np.array([0.1,0.2,0])
-y2 = y1
+        self.init_data()
+        self.init_plot()
 
-#(1,1)
+        self.canvas.Bind(wx.EVT_SCROLLWIN, self.OnScrollEvt)
+        
 
-for i in range(1,64):
-    if i not in (1, 8, 57, 64):
-        plt.subplot(8, 8, i)
-        plt.plot(y1, 'yo-')
-        plt.gca().xaxis.set_major_locator(plt.NullLocator())
-        plt.gca().yaxis.set_major_locator(plt.NullLocator())
+    def init_data(self):
 
-plt.show()
+        # Generate some data to plot:
+        self.dt = 0.01
+        self.t = arange(0,5,self.dt)
+        self.x = sin(2*pi*self.t)
+
+        # Extents of data sequence: 
+        self.i_min = 0
+        self.i_max = len(self.t)
+
+        # Size of plot window:       
+        self.i_window = 100
+
+        # Indices of data interval to be plotted:
+        self.i_start = 0
+        self.i_end = self.i_start + self.i_window
+
+    def init_plot(self):
+        self.axes=[]
+        self.graphs = []
+        for j in range (64):
+            if j not in self.empty:
+                self.axes.append(self.fig.add_subplot(8,8,j+1))
+          
+                self.axes[j].yaxis.set_major_locator(matplotlib.ticker.NullLocator())
+                self.axes[j].xaxis.set_major_locator(matplotlib.ticker.NullLocator())
+                
+                self.graphs.append(
+                      self.axes[j].plot(self.t[self.i_start:self.i_end],
+                                 self.x[self.i_start:self.i_end])[0])
+            else:
+                self.axes.append(0)
+                self.graphs.append(0)
+
+    def draw_plot(self):
+
+        
+        # Adjust plot limits:
+        for i in range (64):
+            if i not in self.empty:
+            # Update data in plot:
+                self.graphs[i].set_xdata(self.t[self.i_start:self.i_end])
+                self.graphs[i].set_ydata(self.x[self.i_start:self.i_end])
+                self.axes[i].set_xlim((min(self.t[self.i_start:self.i_end]),
+                           max(self.t[self.i_start:self.i_end])))
+                self.axes[i].set_ylim((min(self.x[self.i_start:self.i_end]),
+                            max(self.x[self.i_start:self.i_end])))
+                            
+              
+        # Redraw:                  
+        self.canvas.draw()
+
+    def OnScrollEvt(self, event):
+
+        # Update the indices of the plot:
+        self.i_start = self.i_min + event.GetPosition()
+        self.i_end = self.i_min + self.i_window + event.GetPosition()
+        self.draw_plot()
+
+class MyApp(wx.App):
+    def OnInit(self):
+        self.frame = MyFrame(parent=None,id=-1)
+        self.frame.Show()
+        self.SetTopWindow(self.frame)
+        return True
+
+if __name__ == '__main__':
+    app = MyApp()
+    app.MainLoop()
