8a8c2fd641a65232a18394064aae18aff252d393
Nick Beaumont <nob8@cornell.edu>
2013-11-29 15:52:25 -0500

    Beginning work on ConfigParser implementation

--- DataFormatter.py
+++ DataFormatter.py
@@ -2,23 +2,30 @@ import numpy as np
 import h5py
 import os
 import time
+import ConfigParser
 
 
 def timestamp():
     return time.strftime("_%H-%M-%S-0000_%m-%d-%Y_GMT", time.gmtime())
 
 
-def formatData(fileDir, name, *options):
+def formatData(fileDir, name, conf="config.ini", *options):
     """Loads raw numpy arrays from a folder and saves them in hdf5 file format.
         Each array will be given its own dataset under the group 'raw_data'.
 
         Args:
             fileDir: the absolute path to a directory containing one or more numpy files.
-            **options: An optional list of attributes to attach to the raw_data directory
+            name: the name of the hdf5 file to save under. ".hdf5" will be appended to the end.
+            (optional arguments)
+            conf: The name or location of a configuration file associated with this data.
+                  If no root is given in the path, the location will be assumed to be relative
+                  to fileDir.
+            *options: An optional list of attributes to attach to the raw_data directory
     """
     #Decode launch options
     file_access_mode = 'r+' if ('-a' in options) else "w"
     f = h5py.File(name+".hdf5", file_access_mode)
+    #Check if the given file already contains raw_data. If not, create it.
     if(not ('raw_data' in f)):
         data_dir = f.create_group("raw_data")
     else:
@@ -43,7 +50,10 @@ def formatData(fileDir, name, *options):
                 data_dir.attrs.modify("count", [1])
     data_dir.attrs.modify("last_modified", [timestamp()])
     #At this point, the raw datasets have each been imported from the .npy files.
-    #We now check formatting options for important attributes
+    #We now read any metadata from the given configuration file.
+    config = ConfigParser.SafeConfigParser(allow_no_value=True)
+    config.read(conf)
+    #Change directory back to the orignial working directory, in case future commands depend on it
     os.chdir(tmpdir)
     f.flush()
     f.close()
