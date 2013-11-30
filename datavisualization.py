fe292f6d9a68ab8964694958fc4a7dd808bacda9
fatherbobcat <linkena@gmail.com>
2013-11-30 15:11:33 -0500

    8x8 data implemented

--- datavisualization.py
+++ datavisualization.py
@@ -1,6 +1,7 @@
-#TODO: integrate scrollbar in big plot, implement real data, customize graph window
+#TODO: test
 from numpy import arange, sin, pi, float, size
 import datetime
+import math
 import matplotlib
 from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg
 from matplotlib.figure import Figure
@@ -26,6 +27,11 @@ class MyFrame(wx.Frame):
         self.electrodeX=8
         self.electrodeY=8
         
+        if len(data)!=(self.electrodeX*self.electrodeY-len(self.empty)):
+            print "You do not have enough data for electrodes."
+            print "There should be data for 64 electrodes"
+            raise ValueError
+            
         #Data Variables        
         self.data=data
         self.time=time
@@ -72,14 +78,14 @@ class MyFrame(wx.Frame):
         # Generate x axis limits and data intervals:
         self.dt = 1.0/self.samprate
         self.t = arange(0,float(len(self.data[0]))/self.samprate,self.dt)
-        print "There are "+self.t.__repr__()+" time series, and "+self.data[0].__repr__()+" data entries."
+    
         # Extents of data sequence: 
         self.i_min = 0
         self.i_max = len(self.t)
         
         # Size of plot window:       
         self.i_window = self.time*self.samprate
-        print "Window size is "+self.i_window.__repr__()+" entries."
+        
         # Indices of data interval to be plotted:
         self.i_start = 0
         self.i_end = self.i_start + self.i_window
@@ -96,13 +102,14 @@ class MyFrame(wx.Frame):
         
         #Start Time End Time Labels        
         self.startTime = wx.TextCtrl(self.panel, value="Start Time: "+
-            self.i_start.__repr__(), pos=(self.label1x, self.labely), size=(self.labelwidth,-1))
+            (float(self.i_start)/self.samprate).__repr__(), pos=(self.label1x, self.labely), size=(self.labelwidth,-1))
         self.endTime = wx.TextCtrl(self.panel, value="End Time: "+
-            self.i_end.__repr__(), pos=(self.label2x, self.labely), size=(self.labelwidth,-1))        
+            (float(self.i_end)/self.samprate).__repr__(), pos=(self.label2x, self.labely), size=(self.labelwidth,-1))        
         
         #creating each sub plot
         self.axes=[]
         self.graphs = []
+        arrayoffset=0
         for j in range (self.electrodeX * self.electrodeY):
             if j not in self.empty:
                 self.axes.append(self.fig.add_subplot(self.electrodeX,self.electrodeY,j+1))
@@ -110,13 +117,13 @@ class MyFrame(wx.Frame):
                 self.axes[j].yaxis.set_major_locator(matplotlib.ticker.NullLocator())
                 self.axes[j].xaxis.set_major_locator(matplotlib.ticker.NullLocator())
                 
-                self.dataCurrent = self.data[0]
                 self.graphs.append(
                       self.axes[j].plot(self.t[self.i_start:self.i_end],
-                                 self.data[0][self.i_start:self.i_end])[0])
+                                 self.data[j-arrayoffset][self.i_start:self.i_end])[0])
             else:
                 self.axes.append(0)
                 self.graphs.append(0)
+                arrayoffset=arrayoffset+1
         self.canvas.draw()        
             
     """
@@ -125,16 +132,18 @@ class MyFrame(wx.Frame):
     def draw_plot(self):
         
         # Adjust plot limits:
+        arrayoffset=0
         for i in range (self.electrodeX*self.electrodeY):
             if i not in self.empty:
             # Update data in plot:
                 self.graphs[i].set_xdata(self.t[self.i_start:self.i_end])
-                self.graphs[i].set_ydata(self.data[0][self.i_start:self.i_end])
+                self.graphs[i].set_ydata(self.data[i-arrayoffset][self.i_start:self.i_end])
                 self.axes[i].set_xlim((min(self.t[self.i_start:self.i_end]),
                            max(self.t[self.i_start:self.i_end])))
-                self.axes[i].set_ylim((min(self.data[0][self.i_start:self.i_end]),
-                            max(self.data[0][self.i_start:self.i_end])))
-        
+                self.axes[i].set_ylim((min(self.data[i-arrayoffset][self.i_start:self.i_end]),
+                            max(self.data[i-arrayoffset][self.i_start:self.i_end])))
+            else:
+                arrayoffset+=1
         # Redraw:
         self.canvas.draw()
         self.startTime.Refresh()
@@ -149,14 +158,16 @@ class MyFrame(wx.Frame):
             self.draw_plot()
             self.lastupdate = datetime.datetime.utcnow()
         
-        #Update Scrollbar & labels
-        self.canvas.SetScrollPos(wx.HORIZONTAL, event.GetPosition(), True)
-        self.startTime.ChangeValue("Start Time: " + (self.i_start*self.samprate).__repr__())
-        self.endTime.ChangeValue("End Time: " + (self.i_end*self.samprate).__repr__())
-        
         # Update the indices of the plot:
         self.i_start = self.i_min + event.GetPosition()
-        self.i_end = self.i_min + self.i_window + event.GetPosition()
+        self.i_end = self.i_min + self.i_window + event.GetPosition()        
+        
+        #Update Scrollbar & labels
+        self.canvas.SetScrollPos(wx.HORIZONTAL, event.GetPosition(), True)
+        self.startTime.ChangeValue("Start Time: " + (float(self.i_start)/self.samprate).__repr__())
+        self.endTime.ChangeValue("End Time: " + (float(self.i_end)/self.samprate).__repr__())
+        
+       
     
     """
     Handles Graph Scrolling
@@ -173,13 +184,14 @@ class MyFrame(wx.Frame):
         #loop through all plots to check which one was clicked
         i=0
         while i < self.electrodeX*self.electrodeY:
+            arrayoffset=0
             if i not in self.empty:
                 if event.inaxes == self.axes[i]:
                     fig2 = plt.figure()
                     ax_single = fig2.add_subplot(111)
                     
                     #input in data and graph section/ limits
-                    ax_single.plot(self.t, self.data[0], 'yo-')
+                    ax_single.plot(self.t, self.data[i-arrayoffset], 'yo-')
                     ax_single.set_xlim([0,self.time])
                     ax_single.set_autoscale_on(False)
                     
@@ -196,26 +208,66 @@ class MyFrame(wx.Frame):
                         " x "+colno.__repr__())
                     fig2.show()
                     
-                    break                
+                    break
+            else: 
+                arrayoffset+=1               
             i+=1
 
-
 class MyApp(wx.App):
-#    def _init_(self):
-#        self.data = 42
-#        super(MyApp, self)
-   def OnInit(self):
-       #self.frame = MyFrame(parent=None,id=-1, data=self.data)
-       #self.frame.Show()
-       #self.SetTopWindow(self.frame)
-       return True
 
-def analyze8x8data(data, time=1.1, samprate=2):
+   def OnInit(self):
+       return True
+       
+"""
+Function which produces a visualization of 8x8 electrode data with a main
+view (graph of each electrode's data, arranged together according to the 
+electrode positions) and zoom in view (graph of single electrode data). 
+Data = 2D Array of y values to be plotted
+Time (in seconds) = the amount of time the graph should span in each window
+                    should be passed in as an integer
+Samprate = sampling rate, ie how many data samples per second
+          should be passed in as an integer
+"""
+def analyze8x8data(data, time=1, samprate=2):
+   
+   if not (type(time) is int):
+       print "Your 'time' variable is incorrect. Time should be an integer"
+       raise ValueError 
+   if not(type(samprate) is int):
+       print "Your 'samprate' variable is incorrect. Samprate should be an integer"
+       raise ValueError
+       
    app = MyApp()
    frame = MyFrame(parent=None,id=-1, data=data, time=time, samprate=samprate)
    frame.Show()
    app.SetTopWindow(frame)
    app.MainLoop()
 
+"""
+Function which produces visualization of single electrode data. 
+Data = Array of y values to be plotted
+Time (in seconds) = the amount of time the graph should span in each window
+                    should be passed in as an integer
+Samprate = sampling rate, ie how many data samples per second
+        should be passed in as an integer
+"""   
+def analyzesingle(data, time, samprate):
+    
+    if not (type(time) is int):
+       print "Your 'time' variable is incorrect. Time should be an integer"
+       raise ValueError
+    if not(type(samprate) is int):
+       print "Your 'samprate' variable is incorrect. Samprate should be an integer"
+       raise ValueError
+    
+    dt = 1.0/samprate
+    t = arange(0,float(len(data))/samprate,dt)        
+    fig2 = plt.figure()
+    ax_single = fig2.add_subplot(111)
+    ax_single.plot(t, data, 'yo-')
+    ax_single.set_xlim([0,time])
+    ax_single.set_autoscale_on(False)
+
 if __name__ == '__main__':
    analyze8x8data([[1,2,1,4],[2,3,4,5]])
+   analyzesingle([1,2,1,4],1,2)
