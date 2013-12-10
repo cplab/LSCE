bed27a37bea74f79ecbae37881ab985159feadcc
Pedro Rittner <pedro@pedro-alltek-desktop.(none)>
2013-12-10 15:44:04 -0500

    - Cleaned up fullrun implementation to actually use commandline arguments - Added documentation from Google Drive

--- fullrun.py
+++ fullrun.py
@@ -4,15 +4,27 @@ import datavisualization
 import h5py
 import sys
 
+"""
+Usage:
+
+Linux/UNIX/POSIX: 
+$ python fullrun.py /path/to/datadir /path/to/newhdf5file
+
+Windows:
+$ python.exe fullrun.py C:\\path\\to\\rawdata  C:\\path\\to\\newhdf5file
+
+"""
+
 def main(argv=None):
-    Importer.loadFromRaw("E:\\LSCE\\Demo", numFiles = 2)
-    DataFormatter.formatData("E:\\LSCE\\Demo\\slice2_", "E:\\LSCE\\demodata")
-    tmp = h5py.File("fulldata.hdf5", "r+")
+    Importer.loadFromRaw(argv[1], numFiles = 2)
+    DataFormatter.formatData(argv[1], argv[2])
+    tmp = h5py.File("{0}.hdf5".format(argv[2]), "r+")
     data = []
     for dataset in tmp["raw_data"].keys():
         data.append(tmp["raw_data"][dataset])
         print tmp["raw_data"][dataset][0]
     datavisualization.analyze8x8data(data=data, samprate=20000, time=2)
     tmp.close()
+
 if __name__ == "__main__":
-    sys.exit(main())
+    sys.exit(main(argv=sys.argv))
